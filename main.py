8e98ab9035af6766b08f4c0387a1c829bd2f43da
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2025-12-04 01:31:28 +0000

    fix: use 1 worker until migration to model-service architecture to avoid loading models once per thread

--- main.py
+++ main.py
@@ -68,4 +68,10 @@ app.include_router(router)
 if __name__ == "__main__":
     workers = os.cpu_count() or 1
     logger.info("Starting uvicorn with %s workers", workers)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=workers)
+    uvicorn.run(
+        "main:app", 
+        host="0.0.0.0",
+        port=8000,
+        workers=1,
+        reload=False
+    )
